72410e496a7144b62f924e9f31be795833637c2e
Aviral Srivastava <72144346+godofgeeks23@users.noreply.github.com>
2024-05-08 23:59:58 +0530

    Update uploader.sh

--- uploader.sh
+++ uploader.sh
@@ -2,18 +2,11 @@
 
 # Define the directory name
 dirname="imgs"
-
-# for uploading blobs
 upload_token="sp=xxxxxxxxxxxxxxx"
-
-# for reading blobs
 read_token="sp=xxxxxxxxxxxxxxx"
 
-
-# Define the storage URL
 storage_url="https://emailassets1.blob.core.windows.net/mailimages?$upload_token"
 
-# Upload the files
 ./azcopy copy ./"$dirname"/ "$storage_url" --recursive
 
 echo "CDN URLs for uploaded files:"
