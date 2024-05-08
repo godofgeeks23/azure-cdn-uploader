8fbebe2888bfe4e447b38ecdf6b92084508f31a3
Aviral Srivastava <72144346+godofgeeks23@users.noreply.github.com>
2024-05-09 00:01:38 +0530

    Update uploader.sh

--- uploader.sh
+++ uploader.sh
@@ -4,14 +4,16 @@
 dirname="imgs"
 upload_token="sp=xxxxxxxxxxxxxxx"
 read_token="sp=xxxxxxxxxxxxxxx"
+storage_account="emailassets1"
+container_name="mailimages"
 
-storage_url="https://emailassets1.blob.core.windows.net/mailimages?$upload_token"
+storage_url="https://$storage_account.blob.core.windows.net/$container_name?$upload_token"
 
 ./azcopy copy ./"$dirname"/ "$storage_url" --recursive
 
 echo "CDN URLs for uploaded files:"
 for file in ./"$dirname"/*; do
     filename=$(basename "$file")
-    cdn_url="https://cyethackassets.azureedge.net/mailimages/$dirname/$filename?$read_token"
+    cdn_url="https://cyethackassets.azureedge.net/$container_name/$dirname/$filename?$read_token"
     echo "URL for $filename is: $cdn_url"
 done
